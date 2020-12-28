048ff88eb5b079405106809c19ea3daddadc7a9f
takeshisakuma <saude0518@gmail.com>
2020-12-28 22:17:04 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log("e2wwwwwwfcccccccccfvv3");
+var sss = "r3er3q33";
+var ssseerw = "r3er33reee3wq";
+var sssd = function () {
+    console.log("e");
+};
---
+++ ts/main.ts
@@ -0,0 +1,8 @@
+console.log("e2wwwwwwfcccccccccfvv3");
+const sss = "r3er3q33";
+
+const ssseerw = "r3er33reee3wq";
+
+const sssd = () => {
+  console.log("e");
+};
